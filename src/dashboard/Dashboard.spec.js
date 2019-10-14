0a379001ede1a1f5cfbab3f3ffa3ebec946ac9c7
Rayven <rayven.burns@gmail.com>
2019-10-14 17:38:37 -0400

    dahsboard tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
 // Test away
+
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+
+
+
+test('it renders correctly', () => {
+    render(<Dashboard />)
+ });
+
+ test('Contains lock and open ', () => {
+     const { getByText } = render(
+         <Dashboard />);
+    getByText(/locked/i);
+    getByText(/open/i)
+});
+
