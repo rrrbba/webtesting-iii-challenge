200e3ad69bf066fa5b3c57aa357b40d093349335
Rayven <rayven.burns@gmail.com>
2019-10-14 17:19:57 -0400

    controls tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,20 @@
 // Test away!
+
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Controls from './Controls';
+
+test('it renders correctly', () => {
+   render(<Controls />)
+})
+
+test('The door is locked on click', () => {
+    const toggleLockMock = jest.fn();
+    const { getByText } = render (
+        <Controls toggleClosed = {toggleLockMock} />
+    )
+    fireEvent.click(getByText(/close gate/i));
+    expect(toggleLockMock).toHaveBeenCalled()
+
+    
+})
